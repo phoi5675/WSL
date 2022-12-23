ff47ae4c27c9e3df00220eb544d81e7813745a68
phoi5675 <phoi5675@gmail.com>
2022-12-23 11:49:53 +0900

    Fix install wikijs

--- Utils/wiki/install_wikijs.sh
+++ Utils/wiki/install_wikijs.sh
@@ -23,7 +23,7 @@ sudo su -c "psql -c \"ALTER USER wikijs WITH PASSWORD 'wikijsrocks'\"" postgres
 popd
 
 cat >nohup.sh <<EOF
-source ~/.zshrc
+. ~/.nvm/nvm.sh
 nvm use lts/gallium
 cd wiki && node server &
 EOF
