cff946030a452dd80f01ee3fe5054c49cf7e5d7a
phoi5675 <phoi5675@gmail.com>
2023-05-02 16:27:23 +0900

    add *rc backup script

--- misc.sh
+++ misc.sh
@@ -24,3 +24,30 @@ change_user_prompt() {
         exit 0
     fi
 }
+
+backup_and_restore_shell_config() {
+    local BASH_BAK = '.bashrc.bak'
+    local ZSH_BAK = '.zshrc.bak'
+
+    # backup .bashrc
+    if [[ -f "${HOME}/${BASH_BAK}" ]]; then
+        read -p "${BASH_BAK} already exists. do you want to overwrite?[y|N]" PROMPT
+        if [[ ${PROMPT} = "Y" ]] || [[ ${PROMPT} = "y" ]]; then
+            cp "${HOME}/.bashrc" "${HOME}/${BASH_BAK}"
+        fi
+    else
+        echo "backup .bashrc..."
+        cp "${HOME}/.bashrc" "${HOME}/${BASH_BAK}"
+    fi
+
+    # backup .zshrc
+    if [[ -f "${HOME}/${ZSH_BAK}" ]]; then
+        read -p "${ZSH_BAK} already exists. do you want to overwrite?[y|N]" PROMPT
+        if [[ ${PROMPT} = "Y" ]] || [[ ${PROMPT} = "y" ]]; then
+            cp "${HOME}/.bashrc" "${HOME}/${ZSH_BAK}"
+        fi
+    elif [[ ${SET_ZSH} -eq 1 ]]; then
+        echo "backup .zshrc..."
+        cp "${HOME}/.zshrc" "${HOME}/${ZSH_BAK}"
+    fi
+}
